214c6bef09362b8cb249cfd6d28253c3a984b473
丁嘉農 <161096337+CNDING0618@users.noreply.github.com>
2024-03-02 15:28:35 +0800

    Update Lesson6.sql

--- Lesson6.sql
+++ Lesson6.sql
@@ -13,4 +13,16 @@ INSERT INTO student VALUES (5,'小5','英語5',50);
 INSERT INTO student VALUES (6,'小6','英語6',60);
 INSERT INTO student VALUES (7,'小7','英語7',70);
 
+
+/*
+SELECT '欄位名' FROM'表格名' 
+WHERE'比較運算子'
+ORDER BY ASC ^ DESC
+LIMIT '筆數'*/
 SELECT * from student;
+SELECT name, major from student;
+SELECT name AS 姓名, major AS 主修 from student;
+
+SELECT * FROM student ORDER BY score DESC;
+SELECT * FROM student ORDER BY score DESC LIMIT 3;
+SELECT * FROM student ORDER BY score ASC LIMIT 2;
