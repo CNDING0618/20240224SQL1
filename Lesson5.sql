511adb8a32c55422950f240bcd539dc6668c3a72
丁嘉農 <161096337+CNDING0618@users.noreply.github.com>
2024-03-02 14:30:28 +0800

    Update Lesson5.sql

--- Lesson5.sql
+++ Lesson5.sql
@@ -16,3 +16,14 @@ CREATE TABLE student(
 
 SELECT *
 FROM student
+
+INSERT INTO student VALUES (1,'小白','英語')
+
+INSERT INTO student VALUES (2,'小黑','法語')
+RETURNING *;
+
+INSERT INTO student(name,major) VALUES('小灰','歷史')
+RETURNING *;
+
+/*v新增多筆*/
+INSERT INTO student VALUES (3,'小3','英語3'),(4,'小4','英語4'),(5,'小5','英語5'),(6,'小6','英語6'),(7,'小7','英語7')
